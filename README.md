8283455164abfd4f584945d70554986573172032
blis123 <s219708@student.uek.krakow.pl>
2021-05-08 17:58:34 +0200

    2021-05-08

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # CeneoScraper
 ## Etap 1 - Pobranie składowych pojedyńczej opinii o konkretnym produkcie z serwisu [Ceneo.pl](https://www.ceneo.pl)
 1. Pobranie kodu pojedyńczej strony z opiniami o produkcie
+2. Analiza struktury kodu pojedynczej opinii
+
+|Składowa|Selektor CSS|Nazwa zmiennej|Typ danych|
+|:-------||:----------|:-------------|:---------|
+|Opinia|div.js_product-review|opinion|obiekt bs4.e[lement.Tag|
+|Identyfikator opinii|["data-entry-id"]|opinion_id|str|]
+|Autor opinii|span.user-post__author-name|author|str|
+|Rekomendacja|span.user-post__author-recomendation > em|recommendation|bool|
+|Liczba gwiazdek|span.user-post__score-count|stars|float|
+|Treść opinii|div.user-post__text|content|str|
+|Lista zalet|div.review-feature__col:has(> div[class*="positives"]) > div.review-feature__item|pros|list|
+|Lista wad|div.review-feature__col:has(> div[class*="negatives"]) > div.review-feature__item|cons|list|
+|Dla ilu osób przydatna|span[id^="votes-yes"]|useful|int|
+|Dla ilu osób nieprzydatna|span[id^="votes-no"]|useless|int|
+|Czy potwierdzona zakupem|div.review-pz|purchased|bool|
+|Data wystawienia opinii|span.user-post__published > time:nth-child(1)["datetime"]|submit_date|str|
+|Data zakupu produktu|span.user-post__published > time:nth-child(2)["datetime"]|purchase_date|str|
--- scraper.py
+++ scraper.py
@@ -1,4 +1,27 @@
 import requests
+from bs4 import BeautifulSoup
 
 respons = requests.get("https://www.ceneo.pl/71299209#tab=reviews")
-print(respons.text)
+
+page_dom = BeautifulSoup(respons.text, 'html.parser')
+# print(page_dom.prettify())
+
+reviews = page_dom.select("div.js_product-review")
+print(type(reviews))
+review = reviews.pop(0)
+print(type(review))
+review = page_dom.select("div.js_product-review")
+print(type(review))
+
+review_id = review["data-entry-id"]
+author = review.select_one("span.user-post__author-name").text.strip()
+recommendation = review.select_one("span.user-post__author-recomendation").text.strip()
+stars = review.select_one("span.user-post__score-count").text.strip()
+content = review.select_one("div.user-post__text").text.strip()
+pros = review.select("div.review-feature__title--positives ~ div.review-feature__item").text.strip()
+cons = review.select("div.review-feature__title--negatives ~ div.review-feature__item").text.strip()
+useful = review.select_one("button.vote-yes").text.strip()
+useless = review.select_one("button.vote-no]").text.strip()
+purchased = review.select_one("div.review-pz").text.strip()
+review_date = review.select_one("span.user-post__published > time:nth-child(1)["datetime"]").strip()
+purchase_date = review.select_one("span.user-post__published > time:nth-child(2)["datetime"]").text.strip()
